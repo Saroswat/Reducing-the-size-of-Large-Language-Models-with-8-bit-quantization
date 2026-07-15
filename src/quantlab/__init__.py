fac8d4cbb8b3ae9a53b062088c47c88db14b475e
Saroswat Roy <saroswatroy@gmail.com>
2026-07-15 12:17:20 +0100

    feat: add local web lab (src/quantlab/__init__.py)

--- src/quantlab/__init__.py
+++ src/quantlab/__init__.py
@@ -1,9 +1,12 @@
 from .metrics import QuantizationMetrics, compare_tensors
 from .quantization import QuantizedTensor, affine_quantize, symmetric_quantize
 
+__version__ = "0.2.0"
+
 __all__ = [
     "QuantizationMetrics",
     "QuantizedTensor",
+    "__version__",
     "affine_quantize",
     "compare_tensors",
     "symmetric_quantize",
